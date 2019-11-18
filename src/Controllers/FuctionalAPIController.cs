96d295c9ca3923898c4389c4265785e361180678
Shreejan Shrestha <shrestha1shreejan@gmail.com>
2019-11-17 19:12:08 -0600

    Functional API

---
+++ src/Controllers/FuctionalAPIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuctionalAPIController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        #region Constructor
+
+        public FuctionalAPIController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        #endregion
+
+        [HttpOptions("reloadConfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
